940a1ee2b6c9d6caba0a236aafe9792dcfbc069d
Emerson Sena <esena012@gmail.com>
2025-02-07 10:45:22 -0300

    latest

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
-
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:dio/dio.dart';
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:passatempo/models/triva_question_model.dart';
 
@@ -40,34 +40,43 @@ class _HomePageState extends State<HomePage> {
       });
     } catch (e) {
       log('Error fetching questions: $e');
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Erro ao carregar perguntas')));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao carregar perguntas')),
+      );
     }
-    // for (var i in questions) {
-    // log(i.question);
-    // }
   }
 
-//Function to show game over dialog
+  // Function to show game over dialog
   void showGameOverDialog() {
     showDialog(
       context: context,
-      barrierDismissible: false,
+      barrierDismissible: false, // Prevents closing by tapping outside
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("Game Over! 🎉"),
-          content: Text('Your Score: $score/${questions.length}'),
+          title: Text("Game Over! 🎉", textAlign: TextAlign.center),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Your Score: $score/${questions.length}',
+                  style: TextStyle(fontSize: 18)),
+              SizedBox(height: 10),
+              Icon(Icons.emoji_events,
+                  color: const Color.fromARGB(255, 182, 140, 14), size: 50),
+            ],
+          ),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
                 restartGame();
               },
-              child: Text('Play Again'),
+              child: Text('Play Again', style: TextStyle(fontSize: 16)),
             ),
             TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text('Exit'),
+              onPressed: () {
+                SystemNavigator.pop();
+              },
+              child: Text('Exit', style: TextStyle(fontSize: 16)),
             ),
           ],
         );
@@ -75,13 +84,15 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  //Function to restar the game
+  // Function to restart the game
   void restartGame() {
     setState(() {
       current = 0;
       score = 0;
-      getHttp(); //Reload Questions
+      questions = []; // Clear existing questions
     });
+
+    getHttp(); // Fetch new questions
   }
 
   @override
@@ -123,7 +134,7 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
             Row(
@@ -133,12 +144,10 @@ class _HomePageState extends State<HomePage> {
                   width: 50,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(40),
-                    color: const Color.fromARGB(255, 104, 138, 198),
+                    color: Color.fromARGB(255, 104, 138, 198),
                   ),
                 ),
-                SizedBox(
-                  width: 8,
-                ),
+                SizedBox(width: 8),
                 Text(
                   'User',
                   style: GoogleFonts.lato(
@@ -150,25 +159,21 @@ class _HomePageState extends State<HomePage> {
                   style: GoogleFonts.lato(
                       fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(
-                  width: 8,
-                ),
+                SizedBox(width: 8),
                 Text(
-                  '${score}',
+                  '$score',
                   style: GoogleFonts.lato(
                       fontSize: 18, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
-            SizedBox(
-              height: 30,
-            ),
+            SizedBox(height: 30),
             Text(
               'Pergunta ${current + 1}/${questions.length}',
               style: GoogleFonts.workSans(
                 fontSize: 20,
                 fontWeight: FontWeight.w500,
-                color: const Color.fromARGB(255, 46, 114, 48),
+                color: Color.fromARGB(255, 46, 114, 48),
               ),
             ),
             questions.isEmpty
@@ -183,67 +188,58 @@ class _HomePageState extends State<HomePage> {
                     front: TriviaCard(
                       isFront: true,
                       filho: Center(
-                          child: Text(
-                        //questions.isNotEmpty
-                        questions[current].question,
-                        //: 'Loading...',
-                        style: TextStyle(color: Colors.white, fontSize: 24),
-                      )),
+                        child: Text(
+                          questions[current].question,
+                          style: TextStyle(color: Colors.white, fontSize: 24),
+                        ),
+                      ),
                     ),
                     back: TriviaCard(
                       isFront: false,
-                      filho: Center(
-                        child: Column(
-                            children: //questions.isNotEmpty
-                                questions[current]
-                                    .options
-                                    .map((resposta) => ListTile(
-                                          title: Text(
-                                            resposta,
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 16,
-                                            ),
-                                          ),
-                                          onTap: () {
-                                            bool isCorrect = resposta ==
-                                                questions[current]
-                                                    .correctAnswer;
-                                            if (resposta ==
-                                                questions[current]
-                                                    .correctAnswer) {
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(SnackBar(
-                                                      content: Text(
-                                                          'Resposta correta')));
-                                              score++;
-                                              setState(() {
-                                                if (current <
-                                                    questions.length - 1) {
-                                                  current++;
-                                                  cardKey.currentState!
-                                                      .toggleCard();
-                                                }
-                                              });
-                                            } else {
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(SnackBar(
-                                                      content: Text(
-                                                          'Resposta Errada, correta seria: ${questions[current].correctAnswer}')));
-                                              setState(() {
-                                                if (current <
-                                                    questions.length - 1) {
-                                                  current++;
-                                                  cardKey.currentState!
-                                                      .toggleCard();
-                                                }
-                                              });
-                                            }
-                                          },
-                                        ))
-                                    .toList()
-                            //: [],
-                            ),
+                      filho: Column(
+                        children: questions[current]
+                            .options
+                            .map((resposta) => ListTile(
+                                  title: Text(
+                                    resposta,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  onTap: () {
+                                    bool isCorrect = resposta ==
+                                        questions[current].correctAnswer;
+
+                                    if (isCorrect) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                        content: Text('Resposta correta!'),
+                                      ));
+                                      score++;
+                                    } else {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                        content: Text(
+                                          'Resposta Errada! Correta seria: ${questions[current].correctAnswer}',
+                                        ),
+                                      ));
+                                    }
+
+                                    if (current >= questions.length - 1) {
+                                      Future.delayed(
+                                          Duration(milliseconds: 500), () {
+                                        showGameOverDialog();
+                                      });
+                                    } else {
+                                      setState(() {
+                                        current++;
+                                        cardKey.currentState!.toggleCard();
+                                      });
+                                    }
+                                  },
+                                ))
+                            .toList(),
                       ),
                     ),
                   ),
@@ -266,22 +262,23 @@ class TriviaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      duration: Duration(seconds: 5),
-      margin: EdgeInsets.all(5),
-      constraints: BoxConstraints(minHeight: 250),
+      duration: Duration(seconds: 2),
+      margin: EdgeInsets.all(10),
+      constraints: BoxConstraints(minHeight: 200),
       padding: EdgeInsets.all(20),
       width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.8),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: Offset(10, 20),
-            )
-          ],
-          color: isFront ? Colors.brown : Colors.blueGrey,
-          borderRadius: BorderRadius.circular(40)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.8),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(10, 20),
+          )
+        ],
+        color: isFront ? Colors.brown : Colors.blueGrey,
+        borderRadius: BorderRadius.circular(40),
+      ),
       child: filho,
     );
   }
